211ddeafc168c9779917bddb64168c4bb49319c8
thyshimrod <shimrod@free.fr>
2017-10-31 08:11:36 +0100

    Passage en bronze

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ prevy=0
 prevdistance=0
 turn=10
 first = True
+boost = 1
 # game loop
 while True:
     # next_checkpoint_x: x position of the next check point
@@ -17,7 +18,7 @@ while True:
     # next_checkpoint_angle: angle between your pod orientation and the direction of the next checkpoint
     x, y, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_checkpoint_angle = [int(i) for i in input().split()]
     opponent_x, opponent_y = [int(i) for i in input().split()]
-
+   
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr)
     a = x-next_checkpoint_x
@@ -29,19 +30,17 @@ while True:
     vitesse = 100
     if next_checkpoint_x == prevx:
         #if distance > prevdistance and turn < 5:
-        if next_checkpoint_angle > 80 or next_checkpoint_angle < -80:
-            vitesse = 00
+        if next_checkpoint_angle > 100 or next_checkpoint_angle < -100:
+            vitesse = 0
             turn += 1
         else:
-            if distance < 1000:
-                vitesse = 100
-            if not first and distance < 3000:
-                vitesse = 100
-            elif not first and  distance < 5000:
-                vitesse = 100
-
-
-
+            if boost == 1 and next_checkpoint_dist > 6000 and next_checkpoint_angle <10 and next_checkpoint_angle > -10:
+                vitesse = "BOOST"
+                boost-=1
+            elif next_checkpoint_dist < 1000:
+                vitesse = 80
+    
+        
     print(str(next_checkpoint_x) + "//" + str(next_checkpoint_y),file=sys.stderr)
     prevdistance=distance
     prevx = next_checkpoint_x
