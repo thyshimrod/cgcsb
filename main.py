68fd519aacd0f7d25e73a224a3e9b9ce9d84e83e
thyshimrod <shimrod@free.fr>
2017-11-01 15:04:02 +0100

    or

--- main.py
+++ main.py
@@ -4,33 +4,34 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-prevx=-1
+prevx=0
 prevy=0
+prevchkx=-1
+prevchkx=-1
 prevdistance=0
 turn=10
 first = True
 boost = 1
 # game loop
 while True:
-    # next_checkpoint_x: x position of the next check point
-    # next_checkpoint_y: y position of the next check point
-    # next_checkpoint_dist: distance to the next checkpoint
-    # next_checkpoint_angle: angle between your pod orientation and the direction of the next checkpoint
     x, y, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_checkpoint_angle = [int(i) for i in input().split()]
     opponent_x, opponent_y = [int(i) for i in input().split()]
-   
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr)
-    a = x-next_checkpoint_x
-    a=a*a
-    b = y-next_checkpoint_y
-    b=b*b
-    distance = math.sqrt(a+b)
-    print("Debug messages..." + str(distance) + "//" + str(next_checkpoint_dist) + "//" + str(next_checkpoint_angle) , file=sys.stderr)
+    bX = next_checkpoint_x
+    bY = next_checkpoint_y
+
+    if prevx != 0:
+        reachX = (x - prevx)
+        reachY = (y - prevy)
+        length = math.sqrt(reachX * reachX + reachY * reachY)
+        if length != 0:
+            targetX = x +int(reachX / length * next_checkpoint_dist)
+            targetY = y+ int(reachY/ length * next_checkpoint_dist)
+            bX = int(next_checkpoint_x + 0.5*( next_checkpoint_x - targetX))
+            bY = int(next_checkpoint_y + 0.5*( next_checkpoint_y - targetY))
+
     vitesse = 100
-    if next_checkpoint_x == prevx:
-        #if distance > prevdistance and turn < 5:
-        if next_checkpoint_angle > 100 or next_checkpoint_angle < -100:
+    if next_checkpoint_x == prevchkx:
+        if next_checkpoint_angle > 110 or next_checkpoint_angle < -110 and next_checkpoint_dist > 2000:
             vitesse = 0
             turn += 1
         else:
@@ -39,13 +40,12 @@ while True:
                 boost-=1
             elif next_checkpoint_dist < 1000:
                 vitesse = 80
-    
-        
-    print(str(next_checkpoint_x) + "//" + str(next_checkpoint_y),file=sys.stderr)
-    prevdistance=distance
-    prevx = next_checkpoint_x
-    prevy = next_checkpoint_y
-    # You have to output the target position
-    # followed by the power (0 <= thrust <= 100)
-    # i.e.: "x y thrust"
-    print(str(next_checkpoint_x) + " " + str(next_checkpoint_y) + " " + str(vitesse))
+
+
+    prevx = x
+    prevy = y
+
+    prevchkx = next_checkpoint_x
+    prevchky = next_checkpoint_y
+    print(str(bX) + " " + str(bY) + " " + str(vitesse))
+    #print(str(next_checkpoint_x) + " " + str(next_checkpoint_y) + " " + str(vitesse))
